ecb7cdc99db29a10bdb52f90e8dd9aeb8cec8644
umairx97 <umair.bajwa@hotmail.com>
2019-03-18 01:21:45 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ class App extends Component {
       <div className="App">
         <h1>This app is Bootstrapped with Redux starter code</h1>
         <h2>Edit src/App.js and save to reload.</h2>
+        <h3>
+          <u>Structure:</u>
+        </h3>
+
+        <p>
+          <b>Provider / CreateStore</b> is in src/index.js <br />
+          <b>Action Types </b> are in src/actions/types.js <br />
+          <b>Actions</b> are in src/actions/index.js <br />
+          <b>Root Reducer</b> is in src/reducers/index.js <br />
+          <b>Reducers</b> are in src/reducers/dummy.js
+        </p>
       </div>
     );
   }
